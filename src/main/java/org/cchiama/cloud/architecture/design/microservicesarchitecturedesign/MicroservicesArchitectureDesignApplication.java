e799ec5b23097f14fa443741e1169ea1ac9948b2
Christian Chiama <cchiama77@gmail.com>
2024-01-03 17:32:59 +0100

    initial commit

---
+++ src/main/java/org/cchiama/cloud/architecture/design/microservicesarchitecturedesign/MicroservicesArchitectureDesignApplication.java
@@ -0,0 +1,13 @@
+package org.cchiama.cloud.architecture.design.microservicesarchitecturedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesArchitectureDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesArchitectureDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cchiama/cloud/architecture/design/microservicesarchitecturedesign/MicroservicesArchitectureDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cchiama.cloud.architecture.design.microservicesarchitecturedesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesArchitectureDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
